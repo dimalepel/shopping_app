fba5f94bf94c7824379473d83a775f799cfe6e5d
Dimitry Vashkevich <dimalepel@mail.ru>
2023-08-21 15:29:19 +0300

    feat: add bottom navigation bar

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
         child: Text(
-            'Home Screen'
+          'Cart Screen'
         ),
       ),
     );
--- lib/views/favorites_screen.dart
+++ lib/views/favorites_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class FavoritesScreen extends StatelessWidget {
+  const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
         child: Text(
-            'Home Screen'
+            'Favorites Screen'
         ),
       ),
     );
