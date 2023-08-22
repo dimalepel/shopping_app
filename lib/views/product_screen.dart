dfad74c6b4ab2e9f5cfd61e25e16e1ddc803dc5e
Dimitry Vashkevich <dimalepel@mail.ru>
2023-08-22 15:35:26 +0300

    feat: add product details

--- lib/views/product_screen.dart
+++ lib/views/product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fan_carousel_image_slider/fan_carousel_image_slider.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:shopping_app/theme/app_colors.dart';
 
 class ProductScreen extends StatefulWidget {
   const ProductScreen({super.key});
@@ -22,7 +24,7 @@ class _ProductScreenState extends State<ProductScreen> {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Padding(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -30,9 +32,95 @@ class _ProductScreenState extends State<ProductScreen> {
                 SizedBox(
                   width: MediaQuery.of(context).size.width,
                   child: FanCarouselImageSlider(
+                    sliderHeight: 430,
                     imagesLink: images,
                     isAssets: true,
                   ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 30,),
+                        Text(
+                          'Warm Zipper',
+                          style: TextStyle(
+                            color: AppColors.black,
+                            fontWeight: FontWeight.w900,
+                            fontSize: 25,
+                          ),
+                        ),
+                        const SizedBox(height: 10,),
+                        Text(
+                          'Hooded Jacket',
+                          style: TextStyle(
+                            color: AppColors.darkGrey,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      '\$300.00',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: AppColors.orange,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10,),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: RatingBar.builder(
+                    initialRating: 3,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    itemSize: 25,
+                    itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: AppColors.amber,
+                    ),
+                    onRatingUpdate: (rating) { },
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Cool, windy weather is on its way. Send him out\nthe door in a jacket he wants to wear. Warm\nZooper Hooded Jacket.',
+                    style: TextStyle(
+                      color: AppColors.darkGrey,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: AppColors.lightGrey,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.shopping_cart,
+                          color: AppColors.orange,
+                        ),
+                      ),
+                    ),
+                  ],
                 )
               ],
             ),
